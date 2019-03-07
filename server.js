70d9bbf44cb7f918f766274247ffe39915ac7bf5
Allreview <60billion@allrvw.com>
2019-03-07 15:47:21 +0900

    dbTest

--- server.js
+++ server.js
@@ -18,21 +18,22 @@ app.use(methodOverride());
 app.use(cors());
 
 //데이터베이스 접속
-// var mysql = require('mysql');
-// var conn = mysql.createConnection({
-// 	host:"testdatabase.c3asktw2nxxm.ap-northeast-2.rds.amazonaws.com",
-//     user:"root",
-//     password:"11131113",
-//     database:"mockup"
-// })
-// conn.connect(function(){
-//     console.log("connected database!!")
-// });
+var mysql = require('mysql');
+var conn = mysql.createConnection({
+	host:"testdatabase.c3asktw2nxxm.ap-northeast-2.rds.amazonaws.com",
+    user:"root",
+    password:"11131113",
+    database:"arvw"
+})
+conn.connect(function(){
+    console.log("connected database!!")
+});
 
 //이방식으로 라우터를 분리하고자 한다.
 var main = require('./router/main')(app);
 app.use('/main',main);
 
+//ejs 사용방법은 아래 라우터 형식을 따라 가면 이해 할 수 있다.(view/router에 있는 testEjs파일을 확인하세요.)
 var testEjs = require('./router/testEjs')(app);
 app.use('/testEjs',testEjs);
 
