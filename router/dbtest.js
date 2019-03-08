83c49debc21c52f2dbe5cf5834d105c33b28529b
Allreview <60billion@allrvw.com>
2019-03-08 14:28:28 +0900

    dbTest31

--- router/dbtest.js
+++ router/dbtest.js
@@ -1,31 +1,13 @@
 module.exports = function(app)
 {   
   var express = require('express');
-  var bodyparser = require('body-parser');
-  var logger = require('morgan');
-  var methodOverride = require('method-override');
-  var cors = require('cors');
-
-  app.use(logger('dev'));
-  app.use(bodyparser.json());
-  app.use(methodOverride());
-  app.use(cors());
-
-  var mysql = require('mysql');
-  var conn = mysql.createConnection({
-      host:"testdatabase.c3asktw2nxxm.ap-northeast-2.rds.amazonaws.com",
-      user:"root",
-      password:"11131113",
-      database:"mockup"
-  });
-
   var router = express.Router();
 
    //css 및 정적 파일을 사용하기위해서
    router.use(express.static('public'));
 
-   router.post('/dbtest',function(req,res){
-    res.render('dbtest.ejs');
+   router.get('/dbtest',function(req,res){
+    res.render('index.html');
    });
 
    return router;
