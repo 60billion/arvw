e3faa82a99930025ec329cd5d992f20fe0c5a376
Allreview <60billion@allrvw.com>
2019-03-08 14:32:16 +0900

    dbTest34

--- router/dbtest.js
+++ router/dbtest.js
@@ -2,6 +2,14 @@ module.exports = function(app)
 {   
   var express = require('express');
   var router = express.Router();
+  
+  var mysql = require('mysql');
+  var conn = mysql.createConnection({
+    host:"testdatabase.c3asktw2nxxm.ap-northeast-2.rds.amazonaws.com",
+      user:"root",
+      password:"11131113",
+      database:"mockup"
+  });
 
    //css 및 정적 파일을 사용하기위해서
    router.use(express.static('public'));
