cfe1c9b255b47344f3ceb7704ac9bf7f8649dcba
Allreview <60billion@allrvw.com>
2019-03-08 14:23:53 +0900

    dbTest28

--- router/dbtest.js
+++ router/dbtest.js
@@ -1,29 +1,28 @@
 module.exports = function(app)
 {   
-   //app 대신 router를 쓰는이유는 url 정돈이 깔끔할거 같아서
-   var express = require('express');
-   
-   var AWS = require('aws-sdk');
-   AWS.config.region = "ap-northeast-2c";
+  var express = require('express');
+  var bodyparser = require('body-parser');
+  var logger = require('morgan');
+  var methodOverride = require('method-override');
+  var cors = require('cors');
 
+  app.use(logger('dev'));
+  app.use(bodyparser.json());
+  app.use(methodOverride());
+  app.use(cors());
 
-   //mysql접속
-    var mysql = require('mysql');
-    var conn = mysql.createConnection({
-	  host:"testdatabase.c3asktw2nxxm.ap-northeast-2.rds.amazonaws.com",
-    user:"root",
-    password:"11131113",
-    database:"mockup"
-    });
+  var mysql = require('mysql');
+  var conn = mysql.createConnection({
+      host:"testdatabase.c3asktw2nxxm.ap-northeast-2.rds.amazonaws.com",
+      user:"root",
+      password:"11131113",
+      database:"mockup"
+  });
 
-    // conn.connect(function(){
-    //     console.log("connected database in dbtest router");
-    // });
+  var router = express.Router();
 
-   var router = express.Router();
-
-      //css 및 정적 파일을 사용하기위해서
-      router.use(express.static('public'));
+   //css 및 정적 파일을 사용하기위해서
+   router.use(express.static('public'));
 
    router.post('/dbtest',function(req,res){
 
