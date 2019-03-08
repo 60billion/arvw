da261f1cdaee5ffc3440b7017ba2d71de36a7a74
Allreview <60billion@allrvw.com>
2019-03-08 14:33:28 +0900

    dbTest36

--- router/dbtest.js
+++ router/dbtest.js
@@ -14,7 +14,7 @@ module.exports = function(app)
    //css 및 정적 파일을 사용하기위해서
    router.use(express.static('public'));
 
-   router.post('/dbtest',function(req,res){
+   router.get('/dbtest',function(req,res){
      conn.query("select * from user;",function(err,rows,fields){
       if(err){
         console.log(err);
