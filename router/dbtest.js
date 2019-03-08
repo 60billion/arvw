7743dfef60f0111feaa27f9d3e7c34cbd8f6b70c
Allreview <60billion@allrvw.com>
2019-03-08 13:43:15 +0900

    dbTest19

--- router/dbtest.js
+++ router/dbtest.js
@@ -3,7 +3,8 @@ module.exports = function(app)
    //app 대신 router를 쓰는이유는 url 정돈이 깔끔할거 같아서
    var express = require('express');
    var router = express.Router();
-
+   var AWS = require('aws-sdk');
+   AWS.config.region = "ap-northeast-2c";
    //css 및 정적 파일을 사용하기위해서
    router.use(express.static('public'));
 
