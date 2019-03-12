13df13c09ec2e12f37b04e7bfb8188e9445a12aa
Allreview <60billion@allrvw.com>
2019-03-12 16:30:41 +0900

    UI2

--- router/admin.js
+++ router/admin.js
@@ -16,7 +16,7 @@ module.exports = function(app)
    router.use(express.static('public'));
 
   router.get('/upload',function(req,res){
-    res.resnder('admin');
+    res.render('admin');
   });
 
    return router;
