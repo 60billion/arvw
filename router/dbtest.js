6deef1d2a692ff48ff3d32d959b126a4f56f5ab4
Allreview <60billion@allrvw.com>
2019-03-12 15:51:50 +0900

    db,ejs Test1

--- router/dbtest.js
+++ router/dbtest.js
@@ -25,7 +25,7 @@ module.exports = function(app)
         console.log(err);
       }else{
         console.log(rows);
-        res.render('dbtest');
+        res.render('dbtest',rows[0]);
       }
      });
     
